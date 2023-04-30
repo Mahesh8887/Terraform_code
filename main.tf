ba4c0a3b65c5e363a37be286aaa70e5b77b4b6f2
Mahesh8887 <119730245+Mahesh8887@users.noreply.github.com>
2023-05-01 00:56:22 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 terraform {
         required_providers {
+
         aws =  {
                 source = "hashicorp/aws"
                 version = ">= 4.16"
@@ -7,3 +8,29 @@ terraform {
         }
         required_version = ">= 1.2.0"
 }
+
+provider "aws" {
+        region = "us-east-1"
+}
+
+resource "aws_instance" "myfirstinstance" {
+        count = 2
+        ami = "ami-007855ac798b5175e"
+        instance_type = "t2.micro"
+        tags = {
+                Name = "Terraform_Server1"
+                }
+}
+
+resource "aws_s3_bucket" "bucket_form_terraform_by_MVB" {
+        bucket = "aws-s3-bucket.bucket-form-terraform.mahesh"
+        tags = {
+                Name = "bucket_from_terraform_by_mahesh"
+                Environment = "DevQA"
+                }
+}
+
+output "ec2_public_ips" {
+        value = aws_instance.myfirstinstance[*].public_ip
+        }
+                                           
